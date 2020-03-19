fb07004e064b2a79a08468a3e0e7b0009cd90de4
tatphong <50413495+tatphong@users.noreply.github.com>
2020-03-19 18:50:54 +0700

    setup new repos

--- Web/ecommerce/home/views.py
+++ Web/ecommerce/home/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    response = HttpResponse()
-    response.write("<h1>Welcome to ecommerce web</h1>")
-    return response
+    # response = HttpResponse()
+    # response.write("<h1>Welcome to ecommerce web</h1>")
+    return render(request, 'pages/home.html')
+
+def cart(request):
+    return render(request, 'pages/cart.html')
