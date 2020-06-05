7bc9ccbb49619f4fda46cfe166973d22583b98cf
Tat Phong <tattranphong@gmail.com>
2020-06-05 20:56:18 +0700

    complete md5

--- MD5.py
+++ MD5.py
@@ -1,104 +1,110 @@
 import math
+import struct
+from bitarray import bitarray
 import hashlib
-# xác định số dịch chuyển mỗi vòng
-r=[7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,
-    5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,
-    4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,
-    6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21]
-
-# Sử dụng phần nguyên nhị phân của sin của số nguyên làm hằng số:
-k = []
-for i in range(0,63):
-    k.append( math.floor((2**32) * abs(math.sin(i + 1))) )
 
 # Khởi tạo biến:
 h0 = 0x67452301
-h1 = 0xEFCDAB89     #these are int
+h1 = 0xEFCDAB89
 h2 = 0x98BADCFE
 h3 = 0x10325476
 
 # Tiền xử lý:
 # s="The quick brown fox jumps over the lazy dog"
 s = input("Chuỗi cần mã hóa:")
-s = s.encode('utf-8')
-s_hex = s.hex()
-s_bin = bin(int(s_hex, 16)).split('0b')[1]
-    # chặt theo 512 bit
-s_bin_len = len(s_bin)
-chunk_num = s_bin_len//512+1
-    # độn thêm 1000 cho đủ 448 bit
-s_bin+="1"
-while len(s_bin) < chunk_num * 512 - 64:
-    s_bin+="0"
-    # độn 0000 phía trước bit cho đủ 64 bit
-s_bin_len = bin(s_bin_len).split('0b')[1]
-while len(s_bin_len) < 64:
-    s_bin_len = '0'+s_bin_len
-    # kết hợp lại đủ bội số 512 bit
-s_bin += s_bin_len
+bit_array = bitarray(endian="big")
+bit_array.frombytes(s.encode('utf-8'))
+    # độn thêm 1 và 000 cho đủ 448 bit
+bit_array.append(1)
+while bit_array.length() % 512 != 448:
+    bit_array.append(0)
+    # thêm 1 đoạn 64bit thể hiện chiều dài s vào sau dãy bit
+length = (len(s) * 8) % pow(2, 64)
+length_bit_array = bitarray(endian="little")
+length_bit_array.frombytes(struct.pack("<Q", length))
 
-def modular_add(a,b):
-    return (a+b)%pow(2,32)
+result = bitarray(bit_array, endian="little").copy()
+result.extend(length_bit_array)
+# print(result)
 
-def leftrotate(x, c): # x:int, c:int
-    x=bin(x<<c).split("0b")[1]
-    if len(x)>32:
-        a=x[:-32]
-        x=x[-32:]
-        x=x[:-len(a)]
-        x+=a
-    return int(x,2)
+    # chặt theo cụm 512 bit
+bit_array_len = len(result)
+chunk_num = bit_array_len//512
+
+# khai báo các hàm cần thiết
+modular_add = lambda a, b: (a + b) % pow(2, 32)
 rotate_left = lambda x, n: (x << n) | (x >> (32 - n))
-
 def f(index,b,c,d): # all params is int
     if 0<=index<=15:
-        return (b and c) or (not(b) and d)
+        return (b & c) | (~(b) & d)
     if 16<=index<=31:
-        return (d and b) or (not(d) and c)
-    if 32<=index<=48:
+        return (d & b) | (~(d) & c)
+    if 32<=index<=47:
         return b ^ c ^ d
-    if 49<=index<=63:
-        return c ^ (b or not(d))
-    
+    if 48<=index<=63:
+        return c ^ (b | ~(d))
+
+# xác định số dịch chuyển mỗi vòng
+r= [7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,
+    5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,
+    4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,
+    6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21]
+
+# Sử dụng phần nguyên nhị phân của sin của số nguyên làm hằng số:
+k = []
+for i in range(64):
+    k.append( math.floor((2**32) * abs(math.sin(i + 1))) )
+
+#giải
 for i in range(chunk_num):
     # chunk_begin = i*512
     # chunk_end = (i+1)*512-1
-    # ngắt chunk thành 16 đoạn con 32bit
+    # ngắt chunk thành 16 đoạn con 32bit rồi nhân lên thành 64 đoạn
     w = []*64
     for j in range(0, 64):
         if j<16:
             chunk_begin = i*512 + j*32
             # chunk_end = (i+1)*512-1 + (j+1)*32-1
-            w.append(s_bin[chunk_begin:chunk_begin+31])
+            w.append(result[chunk_begin:chunk_begin+32])
         else:
             w.append(w[j-16])
-
+    # Convert the `bitarray` objects to integers.
+    w = [int.from_bytes(word.tobytes(), byteorder="little") for word in w]
     # solve
     a, b, c, d = h0, h1, h2, h3
     # print(hex(b+int(leftrotate(bin(a + f(0,b,c,d) + k[i] + int(w[i],2)).split('0b')[1], r[i]),2)))
-    for i in range(0, 63):
+    for i in range(64):
+        if 0 <= i <= 15 :
+            g = i
+        elif 16 <= i <= 31:
+            g = (5*i + 1) % 16
+        elif 32 <= i <= 47:
+            g = (3*i + 5) % 16
+        elif 48 <= i <= 63:
+            g = (7*i) % 16
         temp = f(i,b,c,d)
         temp = modular_add(temp, a)
-        temp = modular_add(temp, int(w[i],2))
+        temp = modular_add(temp, w[g])
+        # print(temp)
         temp = modular_add(temp, k[i])
-        print("temp: "+str(bin(temp)))
         temp = rotate_left(temp, r[i])
         temp = modular_add(temp, b)
         a = d
         d = c
         c = b
         b = temp
-        # print(str(i)+" / "+hex(b))
-    
+        # print(a,b,c,d)
     h0 = modular_add(h0, a)
     h1 = modular_add(h1, b)
     h2 = modular_add(h2, c)
     h3 = modular_add(h3, d)
-    print(hex(h0)+" "+hex(h1)+" "+hex(h2)+" "+hex(h3))
+    # print(h0,h1,h2,h3)
 
-print(hex(int( (bin(h0).split('0b')[1] + bin(h1).split('0b')[1] + bin(h2).split('0b')[1] + bin(h3).split('0b')[1]) ,2)).split('0x')[1])
-    
+# chuyển 1 chuỗi int thành byte rồi quay về int
+A = struct.unpack("<I", struct.pack(">I", h0))[0]
+B = struct.unpack("<I", struct.pack(">I", h1))[0]
+C = struct.unpack("<I", struct.pack(">I", h2))[0]
+D = struct.unpack("<I", struct.pack(">I", h3))[0]
 
-# print(s_bin)
-# print(w)
-print(hashlib.md5(s).hexdigest())
+print("actual:", f"{format(A, '08x')}{format(B, '08x')}{format(C, '08x')}{format(D, '08x')}")
+print("expected: ",hashlib.md5(s.encode("utf-8")).hexdigest())
