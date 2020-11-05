98370ba456d06853fd1f7eb858db0b7347c020e4
TatPhong <tattranphong@gmail.com>
2020-11-05 10:22:25 +0700

    update hog

--- ML/Img features extraction/hogTest - Copy.py
+++ ML/Img features extraction/hogTest - Copy.py
@@ -4,11 +4,12 @@ from skimage.feature import hog
 from skimage import data, exposure
 import cv2
 
-image = data.astronaut()
-# image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
-# print("shape ", image.shape)
+image_raw = cv2.imread("./images/test.jpg", 0)
+
+image = cv2.cvtColor(image_raw, cv2.COLOR_RGB2GRAY)
+
 fd, hog_image = hog(image, orientations=8, pixels_per_cell=(16, 16),
-                    cells_per_block=(2, 2), visualize=True, multichannel=True)
+                    cells_per_block=(1, 1), visualize=True, multichannel=True)
 
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
 
@@ -22,5 +23,4 @@ hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10))
 ax2.axis('off')
 ax2.imshow(hog_image_rescaled, cmap=plt.cm.gray)
 ax2.set_title('Histogram of Oriented Gradients')
-# print(len(fd), fd)
 plt.show()
